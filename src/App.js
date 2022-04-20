5ab9a528c7a1e7d34bd0d8dc52fc78c54bbd62de
dhimanankita54 <dhimanankita54@gmail.com>
2022-04-20 14:19:48 +0530

    home

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 import ButtonAppBar from "./Components/Navbar";
 import ActionAreaCard from './Components/Offers';
-import  NearMeCarousel  from './Components/Nearme';
-import {Footer} from './Components/Footer';
+import NearMeCarousel from './Components/Nearme';
+import { Footer } from './Components/Footer';
+import { Links } from './Components/Links';
 
 function App() {
   return (
     <div className="App">
-     <ButtonAppBar/>
-     <ActionAreaCard/>
-     <Footer/>
+      <ButtonAppBar />
+      <ActionAreaCard />
+      <Links />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Links.jsx
@@ -0,0 +1,24 @@
+import "./styles/home.css"
+
+export const Links = () => {
+    return (
+        <div className="section-link">
+            <div className="page-link">
+                <img src="https://im1.dineout.co.in/images/uploads/misc/2020/May/8/bffet.png"/>
+                <h4>Buffet</h4>
+            </div>
+            <div className="page-link">
+                <img src="https://im1.dineout.co.in/images/uploads/misc/2020/May/8/pocket_friendly.png"/>
+                <h4>Pocket Friendly</h4>
+            </div>
+            <div className="page-link">
+            <img src="https://im1.dineout.co.in/images/uploads/misc/2020/May/8/must_visit.png"/>
+                <h4>Must Visit</h4>
+            </div>
+            <div className="page-link">
+            <img src="https://im1.dineout.co.in/images/uploads/misc/2020/May/8/new_on_dineout.png"/>
+                <h4>New On Dineout</h4>
+            </div>
+        </div>
+    )
+}
