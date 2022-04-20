2222cc12519879933bbcac592bd767bddfc4d926
dhimanankita54 <dhimanankita54@gmail.com>
2022-04-20 13:25:42 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -41,8 +41,88 @@ export const Footer = () => {
                     </ul>
                 </div>
             </div>
-            <div className="footer-links"></div>
-            <div className="contact"></div>
+            <hr className="sep"></hr>
+            <div className="footer-links">
+                <div className="discover">
+                    <div className="dis-heading">
+                        <h6 className="head">Discover</h6>
+                        <hr className="section-hr"></hr>
+                    </div>
+                    <div className="dis_links">
+                        <li>Trending Restaurants</li>
+                        <li>Super savers</li>
+                        <li>Dineout Pay</li>
+                        <li>Contactless Dining</li>
+                    </div>
+                </div>
+                <div className="about">
+                    <div className="about-heading">
+                        <h6 className="head">About</h6>
+                        <hr className="section-hr"></hr>
+                    </div>
+                    <div className="dis_links">
+                        <li>About us</li>
+                        <li>Blog</li>
+                        <li>Careers</li>
+                        <li>Dineout for Business</li>
+                        <li>Add Restaurant</li>
+                        <li>Terms & Conditions</li>
+                        <li>Privacy Policy</li>
+                    </div>
+                </div>
+                <div className="cuisines">
+                    <div className="about-heading">
+                        <h6 className="head">Top Cuisines</h6>
+                        <hr className="section-hr"></hr>
+                    </div>
+                    <div className="dis_links">
+                        <li>Chinese</li>
+                        <li>Italian</li>
+                        <li>South Indian</li>
+                        <li>Mexican</li>
+                        <li>Bengali</li>
+                        <li>Thai</li>
+                    </div>
+                </div>
+                <div className="facility">
+                    <div className="about-heading">
+                        <h6 className="head">Top Facilities</h6>
+                        <hr className="section-hr"></hr>
+                    </div>
+                    <div className="dis_links">
+                        <li>Fine Dining</li>
+                        <li>5 Star</li>
+                        <li>Rooftop</li>
+                    </div>
+                </div>
+                <div className="location">
+                    <div className="about-heading">
+                        <h6 className="head">Top Locations</h6>
+                        <hr className="section-hr"></hr>
+                    </div>
+                    <div className="dis_links">
+                        <li>Rajouri Garden</li>
+                        <li>Hauz Khass Village</li>
+                        <li>DLF Cyber City</li>
+                        <li>Cannaught Place</li>
+                    </div>
+                </div>
+            </div>
+            <hr className="sep"></hr>
+            <div className="contact">
+                <img className="logo" src='https://im1.dineout.co.in/images/uploads/misc/2019/Jul/25/website-logo.png' />
+                <div className="contact-p">
+                    <p>Find the best Restaurants, Deals, Discounts & Offers</p>
+                    <h6>Contact: +91-9212340202</h6>
+                </div>
+                <div className="social-div">
+                    <img src="https://cdn-icons-png.flaticon.com/128/187/187209.png" />
+                    <img src="https://cdn-icons.flaticon.com/png/128/3670/premium/3670032.png?token=exp=1650441038~hmac=2deb3070fbc811178ed2d5785a96ad83" />
+                    <img src="https://cdn-icons.flaticon.com/png/128/4138/premium/4138124.png?token=exp=1650441119~hmac=f98783553cf5db7d67975cbbc1d075f9" />
+                    <img src="https://cdn-icons-png.flaticon.com/128/145/145812.png" />
+                </div>
+                <p>© 2017, dineout.co.in. All Rights Reserved.</p>
+            </div>
         </div>
     )
 }
